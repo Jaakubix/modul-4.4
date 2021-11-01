1e096530daa539b5f3bbfda71f43a4615153e8e7
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-01 11:42:36 +0100

    fix letters size

--- kalkulator.py
+++ kalkulator.py
@@ -5,19 +5,19 @@ def kalk(oblicz, a, b):
     if oblicz == 1:
         suma = a + b
         logging.info(f"Dodaję {a} i {b}")
-        logging.info(f"wynik to {suma}")
+        logging.info(f"Wynik to {suma}")
     elif oblicz == 2:
         roznica = a - b
         logging.info(f"Odejmuję {a} i {b}")
-        logging.info(f"wynik to {roznica}")
+        logging.info(f"Wynik to {roznica}")
     elif oblicz == 3:
         iloczyn = a * b
         logging.info(f"Mnożę {a} i {b}")
-        logging.info(f"wynik to {iloczyn}")
+        logging.info(f"Wynik to {iloczyn}")
     else:
         iloraz = a / b
         logging.info(f"Dzielę {a} i {b}")
-        logging.info(f"wynik to {iloraz}")
+        logging.info(f"Wynik to {iloraz}")
 
 if __name__ == "__main__":
     oblicz = float(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
