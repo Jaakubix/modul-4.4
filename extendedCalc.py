61583d999793f834c8bd48aa12ac60d431a70386
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-01 16:03:54 +0100

    create extended calculator, import logging, set logging.basicConfig, making choice which mathematical operation you want to do

---
+++ extendedCalc.py
@@ -0,0 +1,5 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == "__main__":
+    oblicz = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
