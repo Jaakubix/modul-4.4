2f1cc212ecd2bb44dee336078448d11b9f986412
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-01 11:06:31 +0100

    add calculations in a function and add function call

--- kalkulator.py
+++ kalkulator.py
@@ -2,9 +2,17 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile.log")
 
 def kalk(oblicz, a, b):
-    
+    if oblicz == 1:
+        suma = a + b
+    elif oblicz == 2:
+        roznica = a - b
+    elif oblicz == 3:
+        iloczyn = a * b
+    else:
+        iloraz = a / b
+
 if __name__ == "__main__":
     oblicz = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
     a = input("Podaj pierwszą liczbę: ")
     b = input("Podaj drugą liczbę: ")
-    
+    kalk(oblicz, a, b)
