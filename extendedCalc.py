1a256d5875ae659d0fe7217160a0797e32c68697
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-01 16:11:22 +0100

    create 4 functions for basic mathematical operations, add inputs in functions

--- extendedCalc.py
+++ extendedCalc.py
@@ -1,5 +1,16 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
+def suma():
+    x = float(input("Podaj liczby oddzielone przecinkami bez używania spacji: "))
+def roznica():
+    a = float(input("Podaj pierwszą liczbę: "))
+    b = float(input("Podaj drugą liczbę: "))
+def iloczyn():
+    x = float(input("Podaj liczby oddzielone przecinkami bez używania spacji: "))
+def iloraz():
+    a = float(input("Podaj pierwszą liczbę: "))
+    b = float(input("Podaj drugą liczbę: "))
+
 if __name__ == "__main__":
     oblicz = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
