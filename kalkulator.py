8c84a761bfafba358298a4bddbce2631d825b2f1
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-01 11:02:26 +0100

    create inputs for function, add logging.basicConfig

--- kalkulator.py
+++ kalkulator.py
@@ -1,4 +1,10 @@
 import logging 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile.log")
 
 def kalk(oblicz, a, b):
+    
+if __name__ == "__main__":
+    oblicz = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+    a = input("Podaj pierwszą liczbę: ")
+    b = input("Podaj drugą liczbę: ")
     
