13eb54a67fc38c9f15e9dee8002162ea21cc4b50
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-01 16:29:54 +0100

    add calculations in a function, add logging.info to print data

--- extendedCalc.py
+++ extendedCalc.py
@@ -2,15 +2,35 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 def suma():
-    x = float(input("Podaj liczby oddzielone przecinkami bez używania spacji: "))
+    x_liczby = input("Podaj liczby oddzielone przecinkami bez używania spacji: ")
+    x = x_liczby.split(',')
+    z = float(x)
+    result = 0
+    for items in z:
+        result = result + items
+    logging.info(f"Dodaję liczby z {x}")
+    logging.info(f"Wynik to {result}")
 def roznica():
     a = float(input("Podaj pierwszą liczbę: "))
     b = float(input("Podaj drugą liczbę: "))
+    result = a - b
+    logging.info(f"Odejmuję {a} i {b}")
+    logging.info(f"Wynik to {result}")
 def iloczyn():
-    x = float(input("Podaj liczby oddzielone przecinkami bez używania spacji: "))
+    x_liczby = input("Podaj liczby oddzielone przecinkami bez używania spacji: ")
+    x = x_liczby.split(',')
+    z = float(x)
+    result = 1
+    for items in z:
+        result = result * items
+    logging.info(f"Mnożę liczby z {x}")
+    logging.info(f"Wynik to {result}")
 def iloraz():
     a = float(input("Podaj pierwszą liczbę: "))
     b = float(input("Podaj drugą liczbę: "))
+    result = a / b
+    logging.info(f"Dzielę {a} i {b}")
+    logging.info(f"Wynik to {result}")
 
 if __name__ == "__main__":
     oblicz = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
